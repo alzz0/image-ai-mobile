165a8213f92469433cf23a6a30b13080e4758f83
TribalScale <alimansour97@gmail.com>
2023-01-05 10:17:32 -0700

    Initial commit: setup up fe & be template + fe navigation

---
+++ imageai-fe/navigation/screens/SettingsScreen.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text
+        style={{ fontSize: 26, fontWeight: "bold" }}
+        onPress={() => navigation.navigate("Home")}
+      >
+        Settings screen
+      </Text>
+    </View>
+  );
+}
