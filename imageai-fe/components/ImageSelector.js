7e7dacf5846989cb442452c841ae265062faa231
TribalScale <alimansour97@gmail.com>
2023-01-08 00:04:53 -0700

    FEAT/ return ai generated image url

--- imageai-fe/components/ImageSelector.js
+++ imageai-fe/components/ImageSelector.js
@@ -14,6 +14,7 @@ import axios from "axios";
 
 function ImageSelector() {
   const [images, setImages] = useState([]);
+  const [aiImageUrl, setAiImageUrl] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const { width } = useWindowDimensions();
   let selectionAmount = 12 - images.length;
@@ -26,7 +27,7 @@ function ImageSelector() {
       // allowsEditing: true,
       allowsMultipleSelection: true,
       selectionLimit: selectionAmount,
-      aspect: [1, 1],
+      aspect: [4, 3],
       quality: 1,
     });
     setIsLoading(false);
@@ -49,13 +50,15 @@ function ImageSelector() {
     const data = {
       key: "QG4CnkidFlqlYIVXVynUEPLKd9JuUiYLRwghDwIzxf1mBDAbSNZngxGqDHhy",
       model_id: "midjourney",
-      prompt: "mdjrny-v4 style space",
-      negative_prompt: null,
-      init_image: `https://imagebucket-imageai.s3.amazonaws.com/${s3Key}`,
+      prompt:
+        "neon cyberpunk man (I((with long blue hair)l)) and cybernetic mechanical parts, conceptart, digital art, in style of darek zabrocki",
+      // "warrior chief, tribal panther make up, blue on red, side profile, looking away, serious eyes, 50mm portrait photography, hard rim lighting photography",
+      negative_prompt: null, //Items you don't want in the image
+      init_image: `https://imagebucket-imageai.s3.amazonaws.com/${s3Key}`, // link of Initial Image
       width: "512",
       height: "512",
-      samples: "1",
-      num_inference_steps: "30",
+      samples: "1", //number of images you want in response
+      num_inference_steps: "30", //: Number of denoising steps (minimum: 1; maximum: 50)
       guidance: null,
       strength: null,
       seed: null,
@@ -66,7 +69,8 @@ function ImageSelector() {
     axios
       .post("https://stablediffusionapi.com/api/v3/dreambooth/img2img", data)
       .then((res) => {
-        console.log("res::", res);
+        console.log("res::", res.data.output);
+        setAiImageUrl(...res.data.output);
       })
       .catch(console.log);
   };
@@ -82,14 +86,15 @@ function ImageSelector() {
       .get(url, { params: { contentType: resp._bodyBlob.type } })
       .then((res) => {
         return res.data;
-      });
+      })
+      .catch(console.log);
     const key = presignedurl.key;
     const result = await fetch(presignedurl.signedUrl, {
       method: "PUT",
       body: imageBody,
     });
     const data = JSON.stringify(result);
-    console.log("result", JSON.stringify(result));
+    console.log("result", data);
     generateAiImg(key);
   };
 
